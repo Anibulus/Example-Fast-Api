433748dbdb8fa2473766bfb47cc403adf6d61fef
Luis Antonio Preza <anibulusnn@gmail.com>
2021-12-28 23:00:00 -0600

    Set valiation fields to models on de api

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 #Python
 from typing import Optional
+from enum import Enum
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 #FastAPI
 from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
+class HairColor(Enum):
+    white= 'white'
+    brown= 'brown'
+    red= 'red'
+
 
 class Location(BaseModel):
     pass
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=100
+    )
+    hair_color: Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = None
 
 #Path Operator  Decorator
